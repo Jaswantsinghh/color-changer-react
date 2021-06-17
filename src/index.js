6e8691d79c949285703e956c85506508988a606c
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-06-17 18:05:10 +0530

    index added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Main extends React.Component {
+    randomColor() {
+        let randColor = Math.floor(Math.random() * 16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randColor;
+        document.querySelector(".input-area").value = "#" + randColor;
+    }
+    render() {
+        return <button onClick={this.randomColor} className="btn">Click me</button>
+    }
+};
+class Heading extends React.Component {
+    render() {
+        return <h1>Color Changer</h1>;
+    }
+};
+class InputColor extends React.Component {
+    render() {
+        return <input className="input-area" placeholder="Color Code"></input>
+    }
+};
+class SubmitBtn extends React.Component {
+    render() {
+        return <button className="btn-sbmt" >Submit</button>;
+    }
+}
+class Body extends React.Component {
+    renderHeading() {
+        return <Heading />
+    }
+    renderMain() {
+        return <Main />
+    }
+    renderinputColor() {
+        return <InputColor />
+    }
+    renderSubmit() {
+        return <SubmitBtn />
+    }
+    render() {
+        return (<div>
+            {this.renderHeading()}
+            {this.renderinputColor()}
+            {this.renderSubmit()}
+            {this.renderMain()}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        </div>
+        )
+    }
+}
+
+const el = document.body;
+ReactDOM.render(<Body />, el);
